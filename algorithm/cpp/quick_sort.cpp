613a3442319be51e43d525dd2187d1478a2bded8
Zhong ziyuan <zzy.albert@163.com>
2016-04-12 22:42:37 +0800

    add quick sort cpp

--- algorithm/cpp/quick_sort.cpp
+++ algorithm/cpp/quick_sort.cpp
@@ -1,12 +1,31 @@
 // Created By:      ZhongZiyuan <zzy.albert@163.com>
 // Created Time:    2016-04-12 22:22:49
-// Modified Time:   2016-04-12 22:22:54
+// Modified Time:   2016-04-12 22:41:24
 
 #include <iostream>
 
 using namespace std;
 
+void partation(int *arr, int begin, int end)
+{
+
+}
+
+void quick_sort(int *arr, int len)
+{
+
+}
+
+void print(int *arr, int len)
+{
+    for(int i=0; i<len; i++)
+        cout << arr[i] << ' ';
+    cout << endl;
+}
+
 int main(int argc, char *argv[]) {
+    int arr[] = {2,5,3,76,12,33};
+    print(arr, 6);
     return 0;
 }
 
