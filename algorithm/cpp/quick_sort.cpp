2773bb30d069e26b1e47d95bf35bca3d324c9d3b
Zhong ziyuan <zzy.albert@163.com>
2016-04-12 22:24:48 +0800

    change c to cpp

---
+++ algorithm/cpp/quick_sort.cpp
@@ -0,0 +1,12 @@
+// Created By:      ZhongZiyuan <zzy.albert@163.com>
+// Created Time:    2016-04-12 22:22:49
+// Modified Time:   2016-04-12 22:22:54
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
+
---
+++ data_structure/cpp/hash_table.cpp
@@ -0,0 +1,12 @@
+// Created By:      ZhongZiyuan <zzy.albert@163.com>
+// Created Time:    2016-04-12 22:23:37
+// Modified Time:   2016-04-12 22:23:39
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
+
