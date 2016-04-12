3509a7fbd2654e638a10ebe781b0414498eec510
Zhong ziyuan <zzy.albert@163.com>
2016-04-12 22:13:09 +0800

    create folder

---
+++ algorithm/c/quick_sort.c
@@ -0,0 +1,10 @@
+// Created By:      ZhongZiyuan <zzy.albert@163.com>
+// Created Time:    2016-04-12 22:12:01
+// Modified Time:   2016-04-12 22:12:09
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
+
