3edf1c583588299c4e2e395e1e98e916b832770d
Zhong ziyuan <zzy.albert@163.com>
2016-06-24 00:05:22 +0800

    b search

--- algorithm/cpp/binary_search.cpp
+++ algorithm/cpp/binary_search.cpp
@@ -1,25 +1,30 @@
 // Created By:      ZhongZiyuan <zzy.albert@163.com>
 // Created Time:    2016-04-12 22:22:49
-// Modified Time:   2016-06-23 19:30:55
+// Modified Time:   2016-06-24 00:04:52
 
 #include <iostream>
-
 using namespace std;
 
-void swap(int& a, int& b)
+int binary_search(int *arr, int len, int value)
 {
-    if(a == b)
-        return;
-    a ^= b;
-    b ^= a;
-    a ^= b;
-}
+    if (NULL == arr || len <= 0)
+        return -1;
 
+    int left = 0;
+    int right = len-1;
+    
+    while (left<=right)
+    {
+        int middle = left + ((right-left)>>1) ;
+        if (arr[middle] > value)
+            right = middle - 1;
+        else if (arr[middle] < value)
+            left = middle + 1;
+        else
+            return middle;
+    }
 
-void binary_search(int *arr, int begin, int end)
-{
-    if (NULL == arr || begin >= end || begin < 0 || end < 0)
-        return;
+    return -1;
 }
 
 /****************************  TEST  ******************************/
@@ -32,11 +37,12 @@ void print(int *arr, int len)
 }
 
 int main(int argc, char *argv[]) {
-    int arr[] = {2,4,21,2,2,15,3,76,52,52,30,12,33,5,5,5};
+    int arr[] = {1,3,4,7,9,12,16,23,27,34,35,45,47,51,55,67};
     int len = 16;
     print(arr, len);
-    quick_sort(arr, 0, len-1);
-    print(arr, len);
+    int value = 12;
+    cout << "The index of "<< value <<" is "
+        <<binary_search(arr, len, value) << endl;
     return 0;
 }
 
