b1a83c897912cda772600f3a328a82f230d09984
Zhong ziyuan <zzy.albert@163.com>
2016-06-23 11:21:24 +0800

    m

--- algorithm/cpp/quick_sort.cpp
+++ algorithm/cpp/quick_sort.cpp
@@ -1,8 +1,10 @@
 // Created By:      ZhongZiyuan <zzy.albert@163.com>
 // Created Time:    2016-04-12 22:22:49
-// Modified Time:   2016-06-16 13:28:15
+// Modified Time:   2016-06-23 11:18:15
 
 #include <iostream>
+//#include <stdlib.h>
+//#include <time.h>
 
 using namespace std;
 
@@ -15,30 +17,56 @@ void swap(int& a, int& b)
     a ^= b;
 }
 
+int random(int begin, int end)
+{
+    if (begin > end)
+        return 0;
+    //srand((long)time(NULL));
+    int r = rand()%(end-begin+1) + begin;
+    return r;
+}
+
 int partition1(int *arr, int begin, int end)
 {
+    swap(arr[begin], arr[random(begin, end)]);
     int mid = begin;
-    for (int i = begin; i <= end; i++ )
+    for (int i = begin + 1; i <= end; i++ )
     {
         if (arr[begin]>=arr[i])
         {
-            swap(arr[mid],arr[i]);
             mid++;
+            swap(arr[mid],arr[i]);
         }
     }
-    mid -= 1;
     swap(arr[begin],arr[mid]);
     return mid;
 }
 
 int partition2(int* arr, int begin, int end)
 {
-    int i=begin, j=end;
-    while(i<j)
+    swap(arr[begin], arr[random(begin, end)]);
+    int i = begin, j = end;
+    int key = arr[begin];
+    while (i<j)
     {
-        while(i<end && arr[i]<=arr[begin]) i++;
-        while(j>begin && arr[j]>arr[begin]) j--;
-        if(i<j)
+        while (i<j && arr[j] > key) j--;
+        arr[i] = arr[j];
+        while (i<j && arr[i] <= key) i++;
+        arr[j] = arr[i];
+    }
+    arr[j] = key;
+    return j;
+}
+
+int partition3(int* arr, int begin, int end)
+{
+    swap(arr[begin], arr[random(begin, end)]);
+    int i=begin, j=end;
+    while (i<j)
+    {
+        while (i<end && arr[i]<=arr[begin]) i++;
+        while (j>begin && arr[j]>arr[begin]) j--;
+        if (i<j)
         {
             swap(arr[i], arr[j]);
             i++;
@@ -51,7 +79,7 @@ int partition2(int* arr, int begin, int end)
 
 void quick_sort(int *arr, int begin, int end)
 {
-    if(NULL == arr || begin >= end || begin < 0 || end < 0)
+    if (NULL == arr || begin >= end || begin < 0 || end < 0)
         return;
     int mid = partition1(arr, begin, end);
     quick_sort(arr, begin, mid-1);
@@ -60,7 +88,7 @@ void quick_sort(int *arr, int begin, int end)
 
 void print(int *arr, int len)
 {
-    for(int i=0; i<len; i++)
+    for (int i=0; i<len; i++)
         cout << arr[i] << ' ';
     cout << endl;
 }
