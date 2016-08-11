65b09f8effc19ecd2dec63548ca2c5bd6102af2d
Zhong ziyuan <zzy.albert@163.com>
2016-08-11 23:42:07 +0800

    java

--- algorithm/java/QuickSort.java
+++ algorithm/java/QuickSort.java
@@ -1,9 +1,40 @@
 class QuickSort{
     
-    public void sort(int[] nums){
+    public static int partition(int[] arr, int begin, int end){
+        int mid = begin;
+        for (int i = begin+1; i<=end; i++){
+            if ( arr[begin] >= arr[i]){
+                mid++;
+                int tmp = arr[mid];
+                arr[mid] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+
+        int tmp = arr[begin];
+        arr[begin] = arr[mid];
+        arr[mid] = tmp;
+        return mid;
+    }
+    
+    public static void sort(int[] arr, int begin, int end){
+        if (null == arr || begin < 0 || begin >=end)
+            return;
+        int mid = partition(arr,begin, end);
+        sort(arr,0, mid-1);
+        sort(arr,mid+1, end);
+    }
+
+    public static void print(int[] a){
+        for (int i=0; i<a.length;i++)
+            System.out.print(a[i]+" ");
+        System.out.println();
     }
 
     public static void main(String[] args){
-        System.out.println("Quick sort!");
+        int[] a = {3,2,2,2,4,12,23,44,12,56};
+        print(a);
+        QuickSort.sort(a,0,a.length-1);
+        print(a);
     }
 }
