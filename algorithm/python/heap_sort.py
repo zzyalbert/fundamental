1e6c577587a4cd781c1a6c98c85c05d2996af184
Zhong ziyuan <zzy.albert@163.com>
2016-08-15 00:08:51 +0800

    python

---
+++ algorithm/python/heap_sort.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Created By:      ZhongZiyuan <zzy.albert@163.com>
+# Created Time:    2016-08-14 23:58:14
+# Modified Time:   2016-08-15 00:08:15
+
+def adjust(arr, begin, end):
+    big_child = begin*2+1
+    if big_child > end:
+        return
+    if big_child+1 <= end and arr[big_child+1]>arr[big_child]:
+        big_child+=1
+    if arr[begin] < arr[big_child]:
+        arr[begin],arr[big_child] = arr[big_child], arr[begin]
+        adjust(arr, big_child, end)
+
+def heap_sort(arr):
+    if None == arr or len(arr)<=1:
+        return
+    begin = 0
+    end = len(arr)-1
+    for i in range((end-1)/2, begin-1):
+        print arr[i]
+        adjust(arr,i,end)
+    for i in range(end, begin):
+        arr[begin],arr[i] = arr[i],arr[begin]
+        adjust(arr, begin, i-1)
+
+if __name__ == "__main__":
+    a = [2,3,2,4,12,23,13,44,2,12]
+    print a
+    heap_sort(a)
+    print a
+
--- algorithm/python/quick_sort.py
+++ algorithm/python/quick_sort.py
@@ -2,8 +2,27 @@
 # coding=utf-8
 # Created By:      ZhongZiyuan <zzy.albert@163.com>
 # Created Time:    2016-04-12 22:16:51
-# Modified Time:   2016-04-12 22:16:56
+# Modified Time:   2016-08-14 23:57:06
+
+def partition(arr, begin, end):
+    mid = begin
+    for i in range(begin+1, end+1):
+        if arr[begin] > arr[i]:
+            mid += 1
+            arr[mid], arr[i] = arr[i], arr[mid]
+    arr[begin], arr[mid] = arr[mid], arr[begin]
+    return mid
+def quick_sort(arr, begin, end):
+    if None == arr or begin <0 or begin >= end:
+        return
+    mid = partition(arr, begin, end)
+    quick_sort(arr, begin, mid-1)
+    quick_sort(arr, mid+1, end)
 
 if __name__ == "__main__":
-    pass
+    a = [2,3,2,5,6,2,21,12,44,5]
+    print a
+    quick_sort(a, 0 , 9)
+    print a
+
 
